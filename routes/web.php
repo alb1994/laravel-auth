bde08c9b13d7215bc58a9373484730348fb8ca52
alb1994 <antonetti_alberto@libero.it>
2023-08-07 19:04:45 +0200

    creata_migration

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,45 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as DashboardController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Qui è possibile registrare le rotte web per la tua applicazione. Queste
+| rotte vengono caricate dal RouteServiceProvider e tutte saranno assegnate al gruppo di middleware "web".
+| Crea qualcosa di fantastico!
 |
 */
 
+// Rotta principale, mostra la vista "welcome"
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Esempio di rotta protetta da autenticazione e verifica email
+// Questa rotta è attualmente commentata
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
+// Definizione delle rotte per l'area amministrativa utilizzando un controller
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+});
+
+// Rotte relative al profilo dell'utente, protette da autenticazione
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Richiede le rotte di autenticazione predefinite fornite da Laravel
 require __DIR__.'/auth.php';
+
+
+
+
